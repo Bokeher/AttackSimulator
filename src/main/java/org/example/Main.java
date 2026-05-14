350a94626d2bd7aceebb8cc486a3e0f520158580
Bokeher <bokeher47@gmail.com>
2026-05-14 20:42:19 +0200

    feat(benchmark): add compareAlgorithms() implementation

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,11 +4,8 @@ import org.example.algorithms.Algorithm;
 import org.example.algorithms.BCryptAlgorithm;
 import org.example.algorithms.MD5Algorithm;
 import org.example.algorithms.SHA1Algorithm;
-import org.example.attacks.Attack;
 import org.example.attacks.BruteForceAttack;
-import org.example.attacks.DictionaryAttack;
 
-import java.io.InputStream;
 
 public class Main {
     public static void main(String[] args) {
@@ -70,7 +67,56 @@ public class Main {
     }
 
     static void compareAlgorithms() {
+        String rawPassword = "j";
+        Charset charset = new Charset.Builder()
+                .lowercase()
+                .build();
 
+        System.out.println("\n=== CHARSET SIZE = " + charset.getChars().length + " ===");
+
+        // 1. NO HASH
+        Algorithm noHash = text -> text;
+
+        String hash1 = noHash.hash(rawPassword);
+
+        System.out.println("\n=== TEST 1: NO HASH (plaintext) ===");
+        new Simulation(
+                new BruteForceAttack(charset.getChars(), 8),
+                noHash
+        ).run(hash1);
+
+        // 2. MD5
+        Algorithm md5 = new MD5Algorithm();
+
+        String hash2 = md5.hash(rawPassword);
+
+        System.out.println("\n=== TEST 2: MD5 ===");
+        new Simulation(
+                new BruteForceAttack(charset.getChars(), 8),
+                md5
+        ).run(hash2);
+
+        // 3. SHA1
+        Algorithm sha1 = new SHA1Algorithm();
+
+        String hash3 = sha1.hash(rawPassword);
+
+        System.out.println("\n=== TEST 3: SHA1 ===");
+        new Simulation(
+                new BruteForceAttack(charset.getChars(), 8),
+                sha1
+        ).run(hash3);
+
+        // 4. BCrypt
+        Algorithm bcrypt = new BCryptAlgorithm();
+
+        String hash4 = bcrypt.hash(rawPassword);
+
+        System.out.println("\n=== TEST 4: BCrypt ===");
+        new Simulation(
+                new BruteForceAttack(charset.getChars(), 8),
+                bcrypt
+        ).run(hash4);
     }
 
     static void compareBCryptCost() {
